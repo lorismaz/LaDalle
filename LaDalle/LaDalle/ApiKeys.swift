5ea6b404d9c78e763104001cb82d97a5ab54dd0e
Loris Mazloum <loris.mazloum@gmail.com>
2016-09-20 09:22:36 -0700

    API Key reader

---
+++ LaDalle/LaDalle/ApiKeys.swift
@@ -0,0 +1,23 @@
+//
+//  ApiKeys.swift
+//  LaDalle
+//
+//  Created by Loris Mazloum on 9/19/16.
+//  Copyright © 2016 Loris Mazloum. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+func valueForAPIKey(named keyname:String) -> String {
+    // Credit to the original source for this technique at
+    // http://blog.lazerwalker.com/blog/2014/05/14/handling-private-api-keys-in-open-source-ios-apps
+    let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist")
+    let plist = NSDictionary(contentsOfFile:filePath!)
+    let value = plist?.object(forKey: keyname) as! String
+    return value
+}
+
+func setValueForAPIKey(named keyname:String, value: String) {
+    
+}
