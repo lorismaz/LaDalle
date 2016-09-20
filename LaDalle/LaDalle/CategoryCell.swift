082fbc54814f9e16292e0ea257a254a7e8cb4648
Loris Mazloum <loris.mazloum@gmail.com>
2016-09-20 09:23:36 -0700

    Created Categories Table View

---
+++ LaDalle/LaDalle/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  LaDalle
+//
+//  Created by Loris Mazloum on 9/19/16.
+//  Copyright © 2016 Loris Mazloum. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LaDalle/LaDalle/CategoryTableViewController.swift
+++ LaDalle/LaDalle/CategoryTableViewController.swift
@@ -11,14 +11,15 @@ import CoreLocation
 
 class CategoryTableViewController: UITableViewController, CLLocationManagerDelegate {
     
+    var categories = Category.loadDefaults()
+    
+    //let searchController = UISearchController(searchResultsController: nil)
+    
     var locationManager: CLLocationManager!
-
     var userCoordinates: Coordinates? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
@@ -38,87 +39,43 @@ class CategoryTableViewController: UITableViewController, CLLocationManagerDeleg
                     
                     let coordinates = Coordinates(latitude: Double(location.coordinate.latitude), longitude: Double(location.coordinate.longitude) )
                     
-                    print("Location \(location.coordinate.latitude) + \(location.coordinate.longitude)")
-                    //print(location )
+                    print("Location \(location.coordinate.latitude) x \(location.coordinate.longitude)")
                     
-                    let yelp = Yelp.instance
-                    yelp.getLocalPlaces(forCategory: "sushis", coordinates: coordinates)
+                    
+                    // move this to
+                    Yelp.sharedInstance.getLocalPlaces(forCategory: "sushi", coordinates: coordinates)
+                    
                     
                 }
             }
         }
     }
-    
-    
-    
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        manager.startUpdatingLocation()
-//        guard let location = manager.location else { return }
-//        print("Location Lat:\(location.coordinate.latitude) / Long: \(location.coordinate.longitude)")
-//    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        let row = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as! CategoryCell
+        
+        // Get current row's category
+        let category = categories[row]
+        
+        // Design the cell
+        cell.categoryNameLabel.text = category.title
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     /*
     // MARK: - Navigation
 
