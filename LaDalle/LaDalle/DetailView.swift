1b3eec54279f5aa9452f271b5c9374c32c5c21a4
Loris Mazloum <loris.mazloum@gmail.com>
2016-09-21 15:47:35 -0700

    Add link to IBInspectable

--- LaDalle/LaDalle/DetailView.swift
+++ LaDalle/LaDalle/DetailView.swift
@@ -9,18 +9,11 @@
 import UIKit
 
 class DetailView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-}
-
-extension UIView {
+    
+    //
+    // https://github.com/rhodgkins/UIView-IBInspectable/blob/master/IBInspectables.swift
+    
+    
     @IBInspectable var cornerRadius: CGFloat {
         get {
             return layer.cornerRadius
