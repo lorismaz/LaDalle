6e4784706c802b1d5dee195a219cc1d1b3486d01
Loris Mazloum <loris.mazloum@gmail.com>
2016-10-20 14:45:36 -0700

    Add unit testing

---
+++ LaDalle/La DalleTests/La_DalleTests.swift
@@ -0,0 +1,43 @@
+//
+//  La_DalleTests.swift
+//  La DalleTests
+//
+//  Created by Loris Mazloum on 10/20/16.
+//  Copyright © 2016 Loris Mazloum. All rights reserved.
+//
+
+import XCTest
+@testable import LaDalle
+
+class La_DalleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetRatingImages() {
+        // This is an example of a functional test case.
+        
+//        let ratingImageName = Business.getRatingImage(rating: 4.5)
+//        
+//        let expected = "rating45"
+//        
+//        XCTAssert(ratingImageName == expected)
+        
+        
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
